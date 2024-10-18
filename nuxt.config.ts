7b3d899a6c9f77e1bf876842385a0f7aeca08b07
Daniel Roe <daniel@roe.dev>
2024-10-18 21:27:18 +0100

    chore: add recommended dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/html-validator',
+    '@nuxt/scripts',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    'nuxt-og-image',
+    'nuxt-time',
+    '@nuxt/test-utils/module',
+  ],
   devtools: { enabled: true },
   future: {
-    compatibilityVersion: 4
-  }
+    compatibilityVersion: 4,
+  },
+  experimental: {
+    typedPages: true,
+  },
+  compatibilityDate: '2024-04-03',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
